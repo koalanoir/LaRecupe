33bc80e4e9de0dd55ddd02c7cdfc979bddb52905
NGOMA SITA <dorian.ngoma-sita@capgemini.com>
2023-09-28 15:38:44 +0200

    add search

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { VendreComponent } from './vendre/vendre.component';
 import { MessagerieComponent } from './messagerie/messagerie.component';
+import { RechercheComponent } from './recherche/recherche.component';
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'vendre', component: VendreComponent },
   { path: 'messagerie', component: MessagerieComponent },
+  { path: 'recherche', component: RechercheComponent }
 ];
 
 
--- src/app/recherche/recherche.component.ts
+++ src/app/recherche/recherche.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recherche.component.scss']
 })
 export class RechercheComponent implements OnInit {
-
+  categorie:string="";
+  etat:string=""
+  min:number=0
+  max:number=0
+  image:string='assets/images/meuble-table.jpg'
   constructor() { }
 
   ngOnInit(): void {
